37f49ee0db4e0cd0b580d396d9a3195c61879ef5
DevSlowz <91501848+DevSlowz@users.noreply.github.com>
2023-11-06 12:41:44 -0500

    Refactor registration form to use new user component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import LoginView from './views/LoginView.vue'
-import Sponsor from './views/Registration/RegistrationSponsor.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+// import LoginView from './views/LoginView.vue'
+// import Sponsor from './views/Registration/RegistrationSponsor.vue'
+import NewUser from './views/Registration/RegistrationNewUser.vue'
 </script>
 
 <template>
 
 
-  <Sponsor />
+  <NewUser />
 
 
   
