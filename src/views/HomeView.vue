17d65bbf31e4190a2d7322541fba91d14cb1bd58
DevSlowz <91501848+DevSlowz@users.noreply.github.com>
2023-11-10 13:11:21 -0500

    Add About Us section to HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,40 @@
-<script setup>
-
-</script>
-
 <template>
-<h1>I am home</h1>
-</template>
+    <div class="flex justify-center items-center h-screen">
+      <div class="center-card">
+        <div class="bg-white rounded-lg shadow-lg p-8 border-2 border-red-500">
+          <h1 class="text-2xl font-bold mb-4">About Us</h1>
+          <p class="text-gray-700">
+            Hot Rides Automotive Expo is the ultimate destination for car and truck enthusiasts across the country. 
+            With events happening nearly every weekend and even on weeknights during the warmer months, 
+            owners have the opportunity to showcase their vehicles and compete for trophies. 
+            Funds raised from these events are carefully managed and budgeted for future expos, 
+            while also being used to support local non-profits and charities. 
+            We are committed to giving back to our communities and making a positive impact through our events. 
+            Additionally, our expos feature vendors and sponsors who contribute to the vibrant atmosphere, 
+            with various sponsorship packages available to promote their brand and support the event. 
+          </p>
+          <br>
+          <p>
+            <b>Join us at Hot Rides Automotive Expo and be part of a community that celebrates the passion for automobiles while giving back to those in need.</b>
+          </p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped>
+    /* Center content vertically and horizontally */
+    .flex {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  
+    /* Add equal padding to the card container */
+    .center-card {
+      padding: 350px; /* You can adjust the padding value as needed */
+    }
+  </style>
+  
