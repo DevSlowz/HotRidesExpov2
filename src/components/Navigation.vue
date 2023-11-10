e982463aee4a2d167c2b769b9515d4bb65d3cbc5
DevSlowz <91501848+DevSlowz@users.noreply.github.com>
2023-11-09 21:50:46 -0500

    Add login functionality to navigation bar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,22 +1,43 @@
 <template>
-    <nav class="bg-gray-800">
-        <div class="nav-container flex justify-between items-center px-4 py-3">
-            <div class="nav-brand">
-                <h1 class="text-white text-2xl font-bold">HotRidesExpo</h1>
-            </div>
-            <div class="nav-links">
-                <ul class="flex flex-1 space-x-4 text-white">
-                    <RouterLink class="cursor-pointer" :to="{name: ''}">Home</RouterLink>
-                    <RouterLink class="cursor-pointer" :to="{name: 'Login'}">Login</RouterLink>
-                    <li class="cursor-pointer">Logout</li>
-                </ul>
-            </div>
+    <header class="bg-gray-800 text-white">
+      <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
+        <div class="flex items-center gap-x-4">
+            <h1 class="text-white text-2xl font-bold">HotRidesExpo</h1>
         </div>
-    </nav>
-</template>
+        <ul class="flex flex-1 justify-end gap-x-10">
+          <router-link class="cursor-pointer" :to="{ name: 'Home' }">Home</router-link>
+          <router-link v-if="user" class="cursor-pointer" :to="{ name: 'Login' }"
+            >Login</router-link
+          >
+          <router-link v-if="!user" class="cursor-pointer" :to="{ name: 'Login' }"
+            >Login</router-link
+          >
+          <li v-if="user" @click="logout" class="cursor-pointer">Logout</li>
+        </ul>
+      </nav>
+    </header>
+  </template>
 
 <script>
+import store from "../stores/index";
+import { computed } from "vue";
+import { supabase } from "../lib/supabaseClient";
+import { useRouter } from "vue-router";
 export default {
-    setup() {}
-}
+  setup() {
+    // Get user from store
+    const user = computed(() => store.state.user);
+
+    // Setup ref to router
+    const router = useRouter();
+
+    // Logout function
+    const logout = async () => {
+      await supabase.auth.signOut();
+      router.push({ name: "Home" });
+    };
+
+    return { logout, user };
+  },
+};
 </script>
