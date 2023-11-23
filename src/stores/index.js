6e0f7955b82ac1c2d2c94d1b66ac07cddbc30e52
DevSlowz <91501848+DevSlowz@users.noreply.github.com>
2023-11-22 19:08:21 -0500

    Refactor user and registration state in stores/index.js, and update PaymentHistory.vue component

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,17 +1,22 @@
 import { reactive } from "vue";
+import { supabase } from "../lib/supabaseClient";
 
 const state = reactive({
   user: null,
-  registrations: []
+  registrations: [],
+  userEmail: null,
+  realUser:null
 });
 
 const methods = {
-  setUser(payload) {
-    
-    state.user = payload ? payload.user : null;
-    console.log("state user : " + JSON.stringify(state.user, null, 2))
-    
-  },
+	setUser(payload) {
+		state.user = payload ? payload.user : null;
+		console.log("state user : " + JSON.stringify(state.user, null, 2));
+		if (state.user) {
+			this.userEmail = (JSON.stringify(state.user.email, null, 2));
+      console.log("User Email" , this.userEmail)
+		}
+	},
   storeRegistration(payload) {
     if (payload) {
       state.registrations.push(payload);
@@ -29,6 +34,28 @@ const methods = {
       return null;
     }
   },
+  async setRealUser(email) {
+		if (!email) {
+			console.error("Email must be provided to fetch user data.");
+			return;
+		}
+		let { data, error } = await supabase
+			.from('Users')
+			.select('*')
+			.eq('Email', email);
+		if (error) {
+			console.error('Error fetching user data: ', error);
+		} else if (data && data.length > 0) {
+			// state.realUser = data[0].UserID;
+      state.realUser = data[0];
+      console.log("real user id: ",state.realUser)
+      return state.realUser
+			// Assuming you might want to call another method after setting realUser
+			// methods.fetchRegisteredEvents(); // You can add fetchRegisteredEvents if needed
+		} else {
+			console.error('No user data found for the provided email.', this.realUser);
+		}
+	},
 };
 
 export default {
--- src/views/PaymentDetails/PaymentHistory.vue
+++ src/views/PaymentDetails/PaymentHistory.vue
@@ -1,35 +1,117 @@
 <template>
-    <div class="container mx-auto px-4">
-      <h1 class="text-2xl font-bold mb-4">Payment History</h1>
-      <table class="table-auto w-full">
-        <thead>
-          <tr>
-            <th class="px-4 py-2">Transaction ID</th>
-            <th class="px-4 py-2">Date</th>
-            <th class="px-4 py-2">Amount</th>
-            <th class="px-4 py-2">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="payment in payments" :key="payment.id">
-            <td class="border px-4 py-2">{{ payment.id }}</td>
-            <td class="border px-4 py-2">{{ payment.date }}</td>
-            <td class="border px-4 py-2">{{ payment.amount }}</td>
-            <td class="border px-4 py-2">{{ payment.status }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        payments: [
-          // Add your payment data here
-        ],
-      };
-    },
-  };
-  </script>
+  <div class="container mx-auto p-4">
+	<h1 class="text-3xl font-semibold mb-6 text-gray-800">Payment History</h1>
+	<button 
+	  @click="goBack"
+	  class="mb-6 bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+	>
+	  ← Back
+	</button>
+	<div class="overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative">
+	  <table class="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
+		<thead>
+		  <tr class="text-left">
+			<th class="bg-gray-800 text-white border-b border-gray-200 px-6 py-4 text-sm uppercase font-bold">
+			  Event Name
+			</th>
+			<th class="bg-gray-800 text-white border-b border-gray-200 px-6 py-4 text-sm uppercase font-bold">
+			  Event Date
+			</th>
+			<th class="bg-gray-800 text-white border-b border-gray-200 px-6 py-4 text-sm uppercase font-bold">
+			  Amount
+			</th>
+			<th class="bg-gray-800 text-white border-b border-gray-200 px-6 py-4 text-sm uppercase font-bold">
+			  Payment Date
+			</th>
+			<th class="bg-gray-800 text-white border-b border-gray-200 px-6 py-4 text-sm uppercase font-bold">
+			  Payment Type
+			</th>
+			<th class="bg-gray-800 text-white border-b border-gray-200 px-6 py-4 text-sm uppercase font-bold">
+			  Payment Method
+			</th>
+		  </tr>
+		</thead>
+		<tbody>
+		  <tr v-for="payment in payments" :key="payment.payment_id" class="hover:bg-gray-100">
+			<td class="border-dashed border-t border-gray-200 px-6 py-3">
+			  <span class="text-gray-700 text-sm">{{ payment.event_name }}</span>
+			</td>
+			<td class="border-dashed border-t border-gray-200 px-6 py-3">
+			  <span class="text-gray-700 text-sm">{{ payment.event_date }}</span>
+			</td>
+			<td class="border-dashed border-t border-gray-200 px-6 py-3">
+			  <span class="text-gray-700 text-sm">{{ payment.payment_amount | currency }}</span>
+			</td>
+			<td class="border-dashed border-t border-gray-200 px-6 py-3">
+			  <span class="text-gray-700 text-sm">{{ payment.payment_date }}</span>
+			</td>
+			<td class="border-dashed border-t border-gray-200 px-6 py-3">
+			  <span class="text-gray-700 text-sm">{{ payment.payment_type }}</span>
+			</td>
+			<td class="border-dashed border-t border-gray-200 px-6 py-3">
+			  <span class="text-gray-700 text-sm">{{ payment.payment_method }}</span>
+			</td>
+		  </tr>
+		</tbody>
+	  </table>
+	</div>
+  </div>
+</template>
+
+<script>
+import { supabase } from "./../../lib/supabaseClient"; // Use the correct path to your Supabase client
+import store from "./../../stores/index";
+export default {
+  data() {
+	return {
+	  payments: []
+	};
+  },
+  methods: {
+	async fetchUserPayments() {
+	  const userEmail = store.state.user.email;
+	  try {
+		const currentUser = await store.methods.setRealUser(userEmail);
+		if (!currentUser) {
+		  throw new Error('User object not found.');
+		}
+		const currentUserID = currentUser.UserID;
+		let { data, error } = await supabase
+		  .from('Registration')
+		  .select(`
+			Payment:payment_id (
+			  payment_date,
+			  payment_type,
+			  payment_method,
+			  payment_amount
+			),
+			Event:event_id (
+			  event_name,
+			  event_date
+			)
+		  `)
+		  .eq('UserID', currentUserID);
+		if (error) throw new Error('Error fetching payments: ' + error.message);
+
+		this.payments = data.map(reg => ({
+		  event_name: reg.Event.event_name,
+		  event_date: reg.Event.event_date,
+		  payment_amount: reg.Payment.payment_amount,
+		  payment_date: reg.Payment.payment_date,
+		  payment_type: reg.Payment.payment_type,
+		  payment_method: reg.Payment.payment_method,
+		  payment_id: reg.Payment.payment_id // Include payment_id to satisfy key requirements for v-for
+		}));
+	  } catch (e) {
+		console.error(e);
+	  }
+	},
+	goBack() {
+	  this.$router.go(-1); // Use Vue Router's go method to navigate back
+	}
+  },
+  created() {
+	this.fetchUserPayments();
+  }
+};
+</script>
